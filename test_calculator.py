84063fb6c7e9a7d1e9eabe503e8c469dde634f5d
Chelsea Li <chelseali2001@gmail.com>
2021-06-03 22:22:10 -0700

    changed test cases

--- test_calculator.py
+++ test_calculator.py
@@ -1,18 +1,14 @@
-import unittest
 import calculator
 
-class SimpleTest(unittest.TestCase):
+class TestCalculatorApp:
     def test(self):
-        self.assertEqual(calculator.add(1, 2), 3)
+        assert 3 == calculator.add(1, 2)
 
     def test0(self):
-        self.assertEqual(calculator.sub(3, 2), 1)
+        assert 1 == calculator.sub(3, 2)
     
     def test1(self):
-        self.assertEqual(calculator.mul(2, 2), 4)
+        assert 4 == calculator.mul(2, 2)
 
     def test2(self):
-        self.assertEqual(calculator.div(4, 2), 2.0)
-
-if __name__ == '__main__':
-    unittest.main()
+        assert 2.0 == calculator.div(4, 2)
